ecdc2a67862caf3c90af528f2c0ae88e6d5edfe3
ebiwonjumit <titoebiwonjumi@gmail.com>
2022-01-28 16:53:42 -0600

    Added basic ShareStory tile, need to add detail page

--- lib/Screens/BottomNavs/FeaturedResourceScreen.dart
+++ lib/Screens/BottomNavs/FeaturedResourceScreen.dart
@@ -1,4 +1,5 @@
 import 'package:bee_foundation_app/Widgets/FeaturedCard.dart';
+import 'package:bee_foundation_app/Widgets/ShareYourStoryCard.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedResourceScreen extends StatefulWidget {
@@ -12,60 +13,72 @@ class FeaturedResourceScreenState extends State<FeaturedResourceScreen> {
     return Container(
         child: SingleChildScrollView(
             child: Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.only(top: 15, left: 20),
-          child: Text(
-            'Featured Resources',
-            style: TextStyle(fontWeight: FontWeight.w700, fontSize: 15),
-          ),
-        ),
-        Padding(
-            padding: EdgeInsets.only(left: 14),
-            child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    FeaturedCard(
-                        text: "Important Info",
-                        webLink: "",
-                        color: Colors.blue, backgroundImage: '',),
-                    FeaturedCard(
-                      text: "More Important Stuff",
-                      webLink: "",
-                      color: Colors.black, backgroundImage: '',
-                    ),
-                    FeaturedCard(
-                      text: "Super Important Info",
-                      webLink: "",
-                      color: Colors.pink, backgroundImage: '',
-                    )
-                  ],
-                ))),
-        Padding(
-            padding: EdgeInsets.only(top: 15, left: 20, bottom: 5),
-            child: Text(
-              'The Statistics',
-              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 15),
-            )),
-        Center(
-          child: Container(
-          margin: EdgeInsets.only(top: 10, left: 10),
-          width: 350,
-          height: 150,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5),
-              image: DecorationImage(image: AssetImage("images/stats.png"))),
-          )),
-        Padding(
-            padding: EdgeInsets.only(top: 15, left: 20, bottom: 5),
-            child: Text(
-              'Share Your Story',
-              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 15),
-            )),
-      ],
-    )));
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 15, left: 20),
+                  child: Text(
+                    'Featured Resources',
+                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
+                  ),
+                ),
+                Padding(
+                    padding: EdgeInsets.only(left: 14),
+                    child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: [
+                            FeaturedCard(
+                              text: "Important Info",
+                              webLink: "",
+                              color: Colors.blue, backgroundImage: '',),
+                            FeaturedCard(
+                              text: "More Important Stuff",
+                              webLink: "",
+                              color: Colors.black, backgroundImage: '',
+                            ),
+                            FeaturedCard(
+                              text: "Super Important Info",
+                              webLink: "",
+                              color: Colors.pink, backgroundImage: '',
+                            )
+                          ],
+                        ))),
+                Padding(
+                    padding: EdgeInsets.only(top: 15, left: 20, bottom: 5),
+                    child: Text(
+                      'The Statistics',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700, fontSize: 18),
+                    )),
+                Center(
+                    child: Container(
+                      margin: EdgeInsets.only(top: 10, left: 10),
+                      width: 350,
+                      height: 150,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          image: DecorationImage(
+                              image: AssetImage("images/stats.png"))),
+                    )),
+                Padding(
+                    padding: EdgeInsets.only(top: 15, left: 20, bottom: 5),
+                    child: Text(
+                      'Share Your Story',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700, fontSize: 18),
+                    )),
+              ListView.builder(
+                      itemCount: 5,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return ShareYourStoryCard(date: "May 4, 2021",
+                          title: "Tricia's Sorbey Story",
+                          description: "The 2021 season opens against",);
+                      }),
+
+              ],
+            )));
   }
 }
---
+++ lib/Widgets/ShareYourStoryCard.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ShareYourStoryCard extends StatelessWidget{
+  final String? date;
+  final String? title;
+  final String? image;
+  final String? description;
+
+  const ShareYourStoryCard(
+  {Key? key,
+    this.date,
+    this.title,
+    this.image,
+    this.description
+  }): super(key: key);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 15,bottom: 5),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                margin: EdgeInsets.only(right: 10),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                     image: ExactAssetImage("images/test_profile.jpg"),
+                    fit: BoxFit.cover
+                  ),
+                  borderRadius: BorderRadius.circular(5.0)
+                ),
+                width: 80,
+                height: 80,
+              )),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 2.0,bottom: 4.0),
+                    child: Text(
+                      date!,
+                      style: TextStyle(fontWeight: FontWeight.w500, color: Colors.grey, fontSize: 14),
+                    ),
+                  ),
+                Text(
+                  title!,
+                  style: TextStyle(color: Colors.black, fontSize: 17, fontWeight: FontWeight.w500),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 2.0, top: 4.0),
+                  child: Text(
+                    description!,
+                    style: TextStyle(color: Colors.grey, fontSize: 13, fontWeight: FontWeight.w500)
+                  ),
+                )
+              ],
+            )
+          ],
+
+        ),
+      ),
+
+      onTap: (){},
+    );
+  }
+
+}
